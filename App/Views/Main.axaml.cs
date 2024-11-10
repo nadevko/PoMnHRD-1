ba638234298d696c43fa3033091e4b1f8e052739
Nadeŭka <nadevko@riseup.net>
2024-11-10 23:58:18 +0300

    Load context in axaml

--- App/Views/Main.axaml.cs
+++ App/Views/Main.axaml.cs
@@ -1,13 +1,30 @@
+using System.Collections.ObjectModel;
 using Avalonia.Controls;
+using Avalonia.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using PMnHRD1.App.Models;
 using PMnHRD1.App.Services;
 
 namespace PMnHRD1.App.Views;
 
 public partial class Main : Window
 {
-    public Main()
+    public Main() => InitializeComponent();
+
+    private void OnSuiteTap(object sender, TappedEventArgs e)
     {
-        InitializeComponent();
-        SuitesList.ItemsSource = Json.Instance.Suites;
+        // if (sender is TextBlock { DataContext: Suite suite })
+        // var suiteView = new SuiteView { DataContext = suite };
     }
+
+    private void OnTestTap(object sender, TappedEventArgs e)
+    {
+        //     if (sender is Grid { DataContext: Test test })
+        // DisplayTestDetails(test);
+    }
+}
+
+public class MainViewModel : ObservableObject
+{
+    public ObservableCollection<Suite> Suites { get; set; } = Json.Instance.Suites;
 }
